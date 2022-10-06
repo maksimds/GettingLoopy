7a1cf83ad5f7fe90573aa7391fdffcb8732a6d28
grotesqueg <denis_maths1@mail.ru>
2022-10-06 13:49:17 -0400

    Loops

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //Part A
+        for (int x = 0; x <= 30; x++)
+        {
+            System.out.println(x);
+        }
+
+        for (int l = 0; l < 3; l++)
+        {
+            System.out.println();
+        }
+
+        for (int x = 30; x >= 0; x--)
+        {
+            System.out.println(x);
+        }
+
+        for (int l = 0; l < 3; l++)
+        {
+            System.out.println();
+        }
+
+        for (int x = 0; x <= 18; x+= 3)
+        {
+            System.out.println(x);
+        }
+
+        for (int l = 0; l < 3; l++)
+        {
+            System.out.println();
+        }
+
+        for (int x = 10; x >= 0; x-= 2)
+        {
+            System.out.println(x);
+        }
     }
 }
